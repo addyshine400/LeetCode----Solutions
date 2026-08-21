d6abac57959069b6c9568535e815561b68c28139
ADITYA RAJ PANDEY <adityaraj6374@gmail.com>
2026-08-21 18:47:07 +0530

    Time: 686 ms (5.01%), Space: 79.8 MB (5.19%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -7,10 +7,13 @@ class Solution {
 
         for (int i = 2; i < n; i++) {
 
-            if (!isComposite[i]) {
-                count++;
-
+            if (!isComposite[i]) { // to check not composite, then it is prime
+                count++; // increase the count
+     
+         //  We only need to start marking multiples from i × i.
                 if ((long) i * i < n) {
+
+                 //   Marks all multiples of i.
                     for (int j = i * i; j < n; j += i) {
                         isComposite[j] = true;
                     }
