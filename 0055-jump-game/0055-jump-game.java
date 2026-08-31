698e3cef8a9fd91558310d7b55e9f2a02c03e2ae
ADITYA RAJ PANDEY <adityaraj6374@gmail.com>
2026-09-01 00:55:06 +0530

    Time: 2 ms (89.03%), Space: 47.8 MB (81.11%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // If current index cannot be reached
+            if (i > maxReach) {
+                return false;
+            }
+
+            // Update the farthest reachable index
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
